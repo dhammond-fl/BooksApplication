19e6caf828eb9f04e8d2ab8cce42bc14f5025a76
dhammond-fl <58090111+dhammond-fl@users.noreply.github.com>
2020-10-11 23:32:04 -0400

    Add files via upload

---
+++ BooksApplication/Models/Books.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+using System.Data.Entity;
+
+namespace BooksApplication.Models
+{
+    public class Book
+    { 
+        public int BookId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        public string Publisher { get; set; }
+
+        public string Details { get; set; }
+
+    }
+
+    public class BookDBContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+
+}
